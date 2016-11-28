9a059f9d4d5f8109f323912159a927d23ccfc1c6
Ovchinnikov <ovchinnikov@smartdriving.io>
2016-11-28 18:56:49 +0300

    Add project files.

---
+++ AzureAppFailsWhenConfigSourceAttributePresents.DemoApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureAppFailsWhenConfigSourceAttributePresents.DemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // This page should never fail, whether there are connection string or app setting defined or some of them is absent
+        public string Index()
+        {
+            return @"<a href=/Home/Demo>Go here</a>";
+        }
+
+        // This page should fail only when there are no appropriate connection string or app setting
+        public string Demo()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DemoConnectionString"].ConnectionString;
+            var appSetting = ConfigurationManager.AppSettings["DemoAppSetting"];
+            var text = string.Join("<br>", $@"<strong>DemoConnectionString</strong>: {connectionString}", $@"<strong>DemoAppSetting</strong>: {appSetting}");
+            return text;
+        }
+    }
+}
---
+++ AzureAppFailsWhenConfigSourceAttributePresents.DemoApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureAppFailsWhenConfigSourceAttributePresents.DemoApp.MvcApplication" Language="C#" %>
